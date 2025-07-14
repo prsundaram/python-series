c0304327e20753eef00f4894db24d4f6d547d9ef
Prasanna Sundaram <prasannaa.sundaram@hotmail.com>
2025-07-14 22:50:09 +0530

    Basic folder structure

---
+++ DataStructures/Lists.py
@@ -0,0 +1,12 @@
+## Creation of list
+
+lst = []
+print(type(lst))
+
+names = ["Prasanna Sundaram", "Sundaram", "Indra"]
+print(names)
+
+#mix list
+mixed_list = [1, 2, "PS", True, 12.5]
+print(mixed_list)
+
